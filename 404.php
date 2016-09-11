ebc177f1ad4f46ee754b2d490100c45b9c280bc1
Tashiro So <saw.fresh846@gmail.com>
2016-09-11 13:03:43 +0900

    CSS変更

--- 404.php
+++ 404.php
@@ -1,41 +1,10 @@
 <?php get_header(); ?>
 	<main>
 		<article>
-			<?php
-				if( have_posts() ){
-					while( have_posts() ){
-						the_post();
-			?>
-			
-			<div id="post-<?php the_ID(); ?>" class="post">
-				<a href="<?php echo esc_url( the_permalink() ); ?>">
-				<h1 class="post-title"><?php the_title(); ?></h1>
-					<p class="post-meta">
-						<span><?php the_date('Y-n-j','更新日: '); ?></span>
-					<?php the_excerpt(); ?>
-					</p>
-				</a>
-			</div>
-			
-			<?php
-					}
-				}else{
-			?>
-			
-			<?php
-				}
-			?>
-			<div class="clearfix"></div>
-			
-			<?php
-				$args = array(
-					'prev_text' => 'PREV',
-					'next_text' => 'NEXT',
-					'show_all' => 'true',
-					'mid_size' => 1,
-				);
-				the_posts_pagination( $args );
-			?>
+			<p style="text-align:center;font-size:20px;">
+			Not found<br>
+			お探しのページはございません。URLをご確認いただくか、メニューより選択してください。
+			</p>
 		</article>
 	</main>
 <?php get_footer(); ?>
