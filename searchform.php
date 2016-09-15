a09d03a9a5af155dcc84000669fe1205df7119cc
Tashiro So <saw.fresh846@gmail.com>
2016-09-15 13:17:52 +0900

    検索を追加してみた

---
+++ searchform.php
@@ -0,0 +1,5 @@
+<form method="get" class="search"
+	action="<?php echo esc_url( home_url('/') ); ?>">
+	<input type="text" placeholder=" サイト内を検索" name="s" class="search_field" value="" />
+	<input type="submit" class="search_button" value="検索" />
+</form>
