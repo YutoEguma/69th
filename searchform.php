050f9630fb19c49e2345c298545d76375dd93358
Yuto Eguma <e20905yehome@gmail.com>
2016-09-19 14:09:15 +0900

    検索フォームを1行にまとめる

--- searchform.php
+++ searchform.php
@@ -1,6 +1,9 @@
-<!-- TODO cabos form を 横並びにしてあげよう -->
+<!-- TODO form を 横並びにしてあげよう -->
 <!-- TODO 検索にはアイコンフォントを用いるとなお良い(虫眼鏡) -->
 <form method="get" class="search" action="<?php echo esc_url(home_url('/')); ?>">
-    <input type="text" placeholder=" キーワード検索" name="s" class="search_field" value=""/>
-    <input type="submit" class="search_button" value="検索"/>
+    <div class="search_text_box">
+        <input type="text" placeholder="フリーワード" name="s" class="search_field" value=""/>
+        <button type="submit" class="search_button" value="検索"><i class="fa fa-search" aria-hidden="true"></i>
+        </button>
+    </div>
 </form>
